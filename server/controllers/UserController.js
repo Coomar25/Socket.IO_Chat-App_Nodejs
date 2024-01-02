ba51fc0272d7d3252bb6577a46685836178d1065
Kumar Chaudhary <kumarbhetwal25@gmail.com>
2024-01-02 13:22:40 +0545

    Completed user authentication

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,28 +1,84 @@
 import User from "../models/userModel.js";
-import bcrypt from "bcrypt";
 import validator from "validator";
+import { createJwtToken } from "../config/generateJwtToken.js";
 
 export const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "All fields required",
+      });
+    }
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({
+        message: "Email must be valid",
+      });
+    }
+    if (!validator.isStrongPassword(password)) {
+      return res.status(400).json({
+        message: "Password must be strong",
+      });
+    }
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+    }
+    // Create new user
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+    });
+    return res.status(201).json({
+      message: "User registered successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
 
-  let user = await User.findOne({ email });
-  if (user) {
-    return res.status(400).json({
-      message: "Email with that user already exist",
-    });
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  const findUser = await User.findOne({ email });
+  if (!findUser || !(await findUser.isPasswordMatched(password))) {
+    return res.status(401).json({ message: "Invalid email or password" });
   }
-  if (!name || !email || !password) {
-    return res.status(400).json({
-      message: "All fields required",
-    });
-  }
+  res.json({
+    token: createJwtToken(findUser?._id),
+    user: {
+      _id: findUser?._id,
+      username: findUser?.username,
+      email: findUser?.email,
+    },
+  });
+};
 
-  if (!validator.isEmail(email)) {
-    return res.status(400).json({
-      message: "Email must need to be valid! dude",
-    });
+export const getSingleUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const singleUser = await User.findById(id);
+    if (!singleUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(singleUser);
+  } catch (error) {
+    throw new Error(error);
   }
-  if (!validator.isStrongPassword(password)) {
-    return res.status(400).json("Password must need to be strong brother");
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await User.find().select("-password");
+    res.json(allUsers);
+  } catch (error) {
+    throw new Error(error);
   }
 };
