8d1466b81798362ad69eb26476b635abe9b617dd
Kumar Chaudhary <kumarbhetwal25@gmail.com>
2024-01-20 22:30:29 +0545

    socket basic

--- zx_six_pack/six_pack_socket/app.js
+++ zx_six_pack/six_pack_socket/app.js
@@ -1,13 +1,44 @@
 import express from "express";
-import Server from "socket.io";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
 
 const app = express();
-const server = new Server(app);
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5174",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
-const io = new Server(server);
+app.use(
+  cors({
+    origin: "http://localhost:5174",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
-// io.
+io.on("connection", (socket) => {
+  console.log("User Connected");
+  console.log("Socket Id", socket.id);
 
-app.listen(8080, () => {
-  console.log(`server is listening at https://localhost:${8080}`);
+  socket.emit("welcome", `Welcome to the server ${socket.id}`);
+  socket.broadcast.emit("broadcasting", `Broadcasting the event ${socket.id}`);
+
+  //    Listen for a custom event from the client
+  socket.on("clientEvent", (data) => {
+    console.log(`Received from a client: ${data}`);
+
+    // send a response back to the client
+    socket.emit("serverResponse", "Server received your message");
+  });
+});
+
+const PORT = 8080;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
 });
